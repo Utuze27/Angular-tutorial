941fb6e2fd4235cd3c2af89581b19008232080ad
Utuze27 <85887542+Utuze27@users.noreply.github.com>
2022-05-09 12:40:57 +0200

    Nuove prove sulla comunicazione tra component e utilizzo stili

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  // Con l'url vado a riprendermi il file css separato in cui posso graficare la pagina web
+  styleUrls: ['./child.component.css'],
+  // Vado ad inserire un nuovo attributo della component per graficare un elemento del html template
+  // styles: ['h2 {color: red}']
+  encapsulation: ViewEncapsulation.None // in questo modo andiamo a ""togliere" l'incapsulamento e possiamo vedere i nostri stili in maniera globale
 })
 export class ChildComponent implements OnInit {
   @Input()
@@ -20,4 +24,8 @@ export class ChildComponent implements OnInit {
   {
    this.messageEvent.emit('Hello from child'); 
   }
+
+  // Proviamo il terzo metodo per far comunicare le components, con la viewchild
+
+  message = 'message from child comp';
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter,ViewChild,AfterViewInit } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
@@ -15,6 +16,14 @@ export class ParentComponent implements OnInit {
   // Metodo per ricevere il messaggio dal child
   receiveMessage(msg: any)
   {
-    alert(msg)
+    alert(msg);
+  }
+
+  //usiamo la viewchild come terzo metodo per far communicare le due components
+  @ViewChild(ChildComponent)child: any; // Uso il decorator della view e vado ad inizilizzare un "oggetto" di tipo Child Component
+
+  ngAfterViewInit()
+  {
+    alert(this.child.message);
   }
 }
